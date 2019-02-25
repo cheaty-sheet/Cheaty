c2b6955f5403d747de1abe2d78dc82bdf2311542
pierre paci <pierre.paci@gmail.com>
2019-02-25 10:18:29 +0100

    fixed cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 
 import * as yargs from "yargs";
-import CheatySheet from "./lib/CheatySheet";
-import * as path from "path";
+import CheatySheet from "./";
 import {basename, extname} from "path";
 import Render from "./lib/modules/renderers/Render.interface";
 
 yargs
-    .command('render <inputs...>', 'render the given input', (yargs) => {
+    .command(['render <inputs...>', '*'], 'render the given input', (yargs) => {
         return yargs.positional('inputs', {
             describe: 'input(s) yaml file',
             type: 'string'
@@ -19,6 +18,7 @@ yargs
             .then((sheet: CheatySheet) => sheet.render('HTML')
                 .then((html: Render) => {
                     const output = basename(input, extname(input)) + '.html';
+                    console.log(`Saving output in ${output}`);
                     html.saveToDisk(output)
                 }))));
     })
